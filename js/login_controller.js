2138ca3964c7b2530b0a6b0dc58be0d8c833814b
Gabriel <gabriel.arriagada.vqz@gmail.com>
2017-08-11 00:58:50 -0400

    Se cambia framework css por bulma, se crea nuevamente la maqueta para el login

--- js/login_controller.js
+++ js/login_controller.js
@@ -6,20 +6,25 @@
 		var vm = this;		
 
 		vm.errorMessage = null;
+		vm.formSubmitted = false;
 
-		vm.doLogin = function(formLogin){
-			if(formLogin.$valid){
-				LoginService.doLogin(vm.user).then(function(response){
-					$cookies.put("access_token", response.data.access_token);
-					$cookies.put("refresh_token", response.data.refresh_token);
-				}).catch(function(error){
-					switch(error.status){
-						case 400: vm.errorMessage = "Credenciales incorrectas"; break;						
-						case 401: vm.errorMessage = "Usuario no autorizado"; break;
-						default: vm.errorMessage = "Ha ocurrido un error al intentar acceder"; break;
-					}
-				});
-			}
+		vm.doLogin = function(formLogin){			
+			if(formLogin.$valid){		
+				vm.formSubmitted = true;			
+				setTimeout(function(){
+					LoginService.doLogin(vm.user).then(function(response){
+						$cookies.put("access_token", response.data.access_token);
+						$cookies.put("refresh_token", response.data.refresh_token);
+					}).catch(function(error){
+						switch(error.status){
+							case 400: vm.errorMessage = "Credenciales incorrectas"; break;						
+							case 401: vm.errorMessage = "Usuario no autorizado"; break;
+							default: vm.errorMessage = "Ha ocurrido un error al intentar acceder"; break;
+						}
+						vm.formSubmitted = false;
+					});
+				}, 2000);
+			}			
 		};
 
 	}]);
